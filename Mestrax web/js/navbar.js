c8d6a6595b9bb00f354e2f1dccf3830484e579df
CubitoEstudioso <ig.lopezf@duocuc.cl>
2026-05-07 03:34:22 -0400

    Se arregla el login activo

--- Mestrax web/js/navbar.js
+++ Mestrax web/js/navbar.js
@@ -7,11 +7,9 @@ if(usuarioLink){
 if(usuarioActivo){
 // Usuario logueado - mostrar link al perfil
 usuarioLink.href = "./perfil.html";
-usuarioLink.textContent = "Usuario";
 } else {
 // Usuario no logueado - mostrar link al login
 usuarioLink.href = "./login.html";
-usuarioLink.textContent = "Usuario";
 }
 }
 
